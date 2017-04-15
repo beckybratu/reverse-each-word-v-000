78a40a61a713ca0d3936472b4b08daeba52e976a
Rebecca Bratu <beckybratu@gmail.com>
2017-04-15 02:51:29 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,8 +6,8 @@ def reverse_each_word(sentence)
     array.join(' ')
 end 
 
-def reverse_each_word(sentence)
-  array = sentence.split
+def reverse_each_word(phrase)
+  array = phrase.split
   array.collect do |word|
     word.reverse!
   end
